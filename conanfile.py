b05ba2b3fbb88668e44db368bf297f26ef55b5e4
Alex Kucher <me@wizard.org.ua>
2018-05-23 19:34:13 +0300

    Enabled NetSSL_OpenSSL in conanfile.py.

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
 import os
 import shutil
-
+ 
 from conans import CMake
 from conans import ConanFile, tools
-
-
+ 
+ 
 class PocoConan(ConanFile):
     name = "Poco"
-    version = "1.8.0"
+    version = "1.8.0.1"
     url = "http://github.com/pocoproject/conan-poco"
     exports_sources = "CMakeLists.txt"
     generators = "cmake", "txt"
@@ -51,7 +51,7 @@ enable_pdf=False
 enable_util=True
 enable_net=True
 enable_netssl=True
-enable_netssl_win=True
+enable_netssl_win=False
 enable_crypto=True
 enable_data=True
 enable_data_sqlite=True
@@ -69,7 +69,7 @@ enable_tests=False
 poco_unbundled=False
 cxx_14=False
 '''
-
+ 
     def source(self):
         zip_name = "poco-%s-release.zip" % self.version
         tools.download("https://github.com/pocoproject/poco/archive/%s" % zip_name, zip_name)
@@ -78,28 +78,27 @@ cxx_14=False
         os.unlink(zip_name)
         shutil.move("poco/CMakeLists.txt", "poco/CMakeListsOriginal.cmake")
         shutil.move("CMakeLists.txt", "poco/CMakeLists.txt")
-
+ 
     def configure(self):
         if self.options.enable_apacheconnector:
             raise Exception("Apache connector not supported: https://github.com/pocoproject/poco/issues/1764")
-
+ 
     def requirements(self):
         if self.options.enable_netssl or self.options.enable_netssl_win or self.options.enable_crypto or self.options.force_openssl:
             self.requires.add("OpenSSL/1.0.2l@conan/stable", private=False)
-
+ 
         if self.options.enable_data_mysql:
-            # self.requires.add("MySQLClient/6.1.6@hklabbers/stable")
-            raise Exception("MySQL not supported yet, open an issue here please: %s" % self.url)
-
+            self.requires.add("mysql-connector-c/6.1.11@avantize/stable")
+ 
     def build(self):
         if self.settings.compiler == "Visual Studio" and self.options.shared:
             self.output.warn("Adding ws2_32 dependency...")
             replace = 'Net Util Foundation Crypt32.lib'
             tools.replace_in_file("poco/NetSSL_Win/CMakeLists.txt", replace, replace + " ws2_32 ")
-
+ 
             replace = 'Foundation ${OPENSSL_LIBRARIES}'
             tools.replace_in_file("poco/Crypto/CMakeLists.txt", replace, replace + " ws2_32 Crypt32.lib")
-
+ 
         cmake = CMake(self, parallel=None)  # Parallel crashes building
         for option_name in self.options.values.fields:
             activated = getattr(self.options, option_name)
@@ -107,14 +106,14 @@ cxx_14=False
                 cmake.definitions["POCO_STATIC"] = "OFF" if activated else "ON"
             else:
                 cmake.definitions[option_name.upper()] = "ON" if activated else "OFF"
-
+ 
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":  # MT or MTd
             cmake.definitions["POCO_MT"] = "ON" if "MT" in str(self.settings.compiler.runtime) else "OFF"
         self.output.info(cmake.definitions)
         os.mkdir("build")
         cmake.configure(source_dir="../poco", build_dir="build")
         cmake.build()
-
+ 
     def package(self):
         # Copy the license files
         self.copy("poco/LICENSE", dst=".", keep_path=False)
@@ -122,14 +121,14 @@ cxx_14=False
         packages = ["CppUnit", "Crypto", "Data", "Data/MySQL", "Data/ODBC", "Data/SQLite",
                     "Foundation", "JSON", "MongoDB", "Net", "Util",
                     "XML", "Zip"]
-        if self.settings.os == "Windows":
+        if self.settings.os == "Windows" and self.options.force_openssl == False:
             packages.append("NetSSL_Win")
         else:
             packages.append("NetSSL_OpenSSL")
-
+ 
         for header in packages:
             self.copy(pattern="*.h", dst="include", src="poco/%s/include" % header)
-
+ 
         # But for libs and dlls, we want to avoid intermediate folders
         self.copy(pattern="*.lib", dst="lib", src="build/lib", keep_path=False)
         self.copy(pattern="*.a",   dst="lib", src="build/lib", keep_path=False)
@@ -137,7 +136,7 @@ cxx_14=False
         # in linux shared libs are in lib, not bin
         self.copy(pattern="*.so*", dst="lib", src="build/lib", keep_path=False, symlinks=True)
         self.copy(pattern="*.dylib", dst="lib", src="build/lib", keep_path=False)
-
+ 
     def package_info(self):
         """ Define the required info that the consumers/users of this package will have
         to add to their projects
@@ -158,24 +157,24 @@ cxx_14=False
                 ("enable_apacheconnector", "PocoApacheConnector"),
                 ("enable_xml", "PocoXML"),
                 ("enable_json", "PocoJSON")]
-
+ 
         suffix = str(self.settings.compiler.runtime).lower()  \
                  if self.settings.compiler == "Visual Studio" and not self.options.shared \
                  else ("d" if self.settings.build_type=="Debug" else "")
         for flag, lib in libs:
             if getattr(self.options, flag):
-                if self.settings.os == "Windows" and flag == "enable_netssl":
+                if self.settings.os == "Windows" and flag == "enable_netssl" and self.options.force_openssl == False:
                     continue
                 if self.settings.os != "Windows" and flag == "enable_netssl_win":
                     continue
                 self.cpp_info.libs.append("%s%s" % (lib, suffix))
-
+ 
         self.cpp_info.libs.append("PocoFoundation%s" % suffix)
-
+ 
         # in linux we need to link also with these libs
         if self.settings.os == "Linux":
             self.cpp_info.libs.extend(["pthread", "dl", "rt"])
-
+ 
         if not self.options.shared:
             self.cpp_info.defines.extend(["POCO_STATIC=ON", "POCO_NO_AUTOMATIC_LIBS"])
             if self.settings.compiler == "Visual Studio":
